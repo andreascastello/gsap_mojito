71de6d4949536219e0cc1bb9c6c6e9a25134e15e
andreascastello <andreas.castello06@gmail.com>
2025-11-21 15:20:12 +0100

    implement cocktail section

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { ScrollTrigger, SplitText } from 'gsap/all'
 import Navbar from './components/Navbar'
 import Heros from './components/Heros'
-
+import Cocktails from './components/Cocktails'
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
@@ -11,6 +11,7 @@ const App = () => {
     <main>
       <Navbar />
       <Heros />
+      <Cocktails />
     </main>
   )
 }
